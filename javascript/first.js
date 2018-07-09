4de2d9f66844dbfadb0bb4750e119548d766b8e0
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-07-09 12:36:44 +0530

    A

---
+++ javascript/first.js
@@ -0,0 +1,137 @@
+V8
+
+Es3  EcmaScript
+Es5   
+Es6
+Es7
+
+DATATYPES
+String  "javascript" 'angular' "react" '10'
+Number  3 465 7678 798 0
+Boolean true false
+
+
+var name = "javascript"
+undefined
+name
+"javascript"
+var name="angular"
+undefined
+name
+"angular"
+let subject="english"
+undefined
+subject
+"english"
+let subject="hindi"
+VM82:1 Uncaught SyntaxError: Identifier 'subject' has already been declared
+    at <anonymous>:1:1
+(anonymous) @ VM82:1
+subject = "hindi"
+"hindi"
+const city = "london"
+undefined
+city
+"london"
+const city = "newyork"
+VM96:1 Uncaught SyntaxError: Identifier 'city' has already been declared
+    at <anonymous>:1:1
+(anonymous) @ VM96:1
+city = "newyork"
+VM100:1 Uncaught TypeError: Assignment to constant variable.
+    at <anonymous>:1:6
+(anonymous) @ VM100:1
+city
+"london"
+
+
+
+///////////////////
+var a = 10
+undefined
+a
+10
+var a = "10"
+undefined
+a
+"10"
+typeof(a)
+"string"
+var b = 10
+undefined
+typeof(b)
+"number"
+
+
+
+//////////////
+
+var a = 10
+undefined
+var b = 10
+undefined
+a+a
+20
+a-a
+0
+a/a
+1
+a*a
+100
+a*b
+100
+10%2
+0
+10%3
+1
+
+
+///////////////////
+var first = "java"
+undefined
+var second = "script"
+undefined
+first+second
+"javascript"
+first-second
+NaN
+first*second
+NaN
+first/second
+NaN
+isNaN("aaaa")
+true
+isNaN(10)
+false
+
+//////////////////////
+var a = "10"
+undefined
+var b = "20"
+undefined
+a+b
+"1020"
+parseInt(a)+parseInt(b)
+30
+a*b
+200
+a/b
+0.5
+var c = 10
+undefined
+c.toString
+ƒ toString() { [native code] }
+c.toString()
+"10"
+""+c
+"10"
+var a += +"10"
+10
+www.zoom.us/j/123
++route.url
+var a +=1
+
+a +=1
+"101"
+
+
---
+++ javascript/second.js
@@ -0,0 +1,55 @@
+var a = "Javascript"
+undefined
+a.toUpperCase()
+"JAVASCRIPT"
+a.toLowerCase()
+"javascript"
+"Angular Version"
+"Angular Version"
+a.length
+10
+a.toUpperCase
+ƒ toUpperCase() { [native code] }
+a.charAt(0)
+"J"
+var a = "london"
+undefined
+a.slice(1)
+"ondon"
+a.slice(0)
+"london"
+a.charAt(0).toUpperCase()
+"L"
+a.charAt(0).toUpperCase()+a.slice(1)
+"London"
+a.slice(2)
+"ndon"
+a.slice(4)
+"on"
+a.slice(1,3)
+"on"
+////////////////////////////
+var a = "i am doing javascript"
+undefined
+a.indexOf("a")
+2
+a.replace("javascript","angular")
+"i am doing angular"
+a.split(' ')
+(4) ["i", "am", "doing", "javascript"]
+var href="https://www.youtube.com/watch?v=zq2oPP6I0NE&t=162s"
+undefined
+href.split("?")
+(2) ["https://www.youtube.com/watch", "v=zq2oPP6I0NE&t=162s"]
+var a = "hi"
+undefined
+var b =" good"
+undefined
+a.concat(b)
+"hi good"
+b.concat(a)
+" goodhi"
+var c = "hello"
+undefined
+a.concat(b,c)
+"hi goodhello"
