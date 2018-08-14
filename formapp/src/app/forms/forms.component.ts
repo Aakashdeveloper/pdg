6c5c0b7e7129da3498fb6c395e936553c63bd93d
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-08-14 09:54:51 +0530

    a

--- formapp/src/app/forms/forms.component.ts
+++ formapp/src/app/forms/forms.component.ts
@@ -8,18 +8,36 @@ import {Employee} from '../models/employee.model';
 })
 export class FormsComponent implements OnInit {
 
-  languages = ['AngularJS', 'ReactJs', "NodeJS"]
-  model = new Employee('John','','',true,'')
+  languages = ['AngularJS', 'ReactJs', 'NodeJS'];
+  model = new Employee('John', 'Andrew', '', '',  'male', true, '');
+  hasCodeLangError: Boolean = false;
+  disbableSubmit: Boolean = false;
 
-  firstToUpper(value:string): void{
-    if(value.length>0)
-      this.model.firstName = value.charAt(0).toUpperCase()+value.slice(1)
-    else
-      this.model.firstName = value
+  firstToUpper(value: string): void  {
+    if (value.length > 0) {
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    } else {
+      this.model.firstName = value;
+    }
+  }
+
+  validateCodeLang(event): void {
+    if (this.model.codelang === 'default') {
+      this.hasCodeLangError = true;
+      this.disbableSubmit = true;
+    } else {
+      this.hasCodeLangError = false;
+      this.disbableSubmit = false;
+    }
   }
   constructor() { }
 
   ngOnInit() {
   }
-
 }
+
+
+/*
+ng-pristine  ng-touched     ng-valid
+ng-dirty     ng-untouched   ng-invalid
+*/
--- formapp/src/app/models/employee.model.ts
+++ formapp/src/app/models/employee.model.ts
@@ -2,6 +2,8 @@ export class Employee {
     constructor(
         public firstName: string,
         public lastName: string,
+        public email: string,
+        public password: string,
         public gender: string,
         public fulltime: boolean,
         public codelang: string
