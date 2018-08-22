fab1e860979e180c83108dd8c238613eff9c5b8a
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-08-22 05:46:10 +0100

    A

--- reactiveforms/src/app/customers/customer.component.ts
+++ reactiveforms/src/app/customers/customer.component.ts
@@ -1,8 +1,30 @@
 import { Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl, FormBuilder,
-         Validators} from '@angular/forms';
+         Validators, ValidatorFn, AbstractControl} from '@angular/forms';
 
 import { Customer } from './customer';
+
+
+function ratingRange(min: number, max: number): ValidatorFn {
+    return(c: AbstractControl): {[key: string]: boolean} | null => {
+        if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return{'range1': true};
+        }
+        return null;
+    };
+}
+
+function emailMatcher(c: AbstractControl) {
+    const emailControl = c.get('email');
+    const confirmControl = c.get('confirmEmail');
+    if (emailControl.pristine || confirmControl.pristine) {
+        return null;
+    }
+    if (emailControl.value === confirmControl.value) {
+        return null;
+    }
+    return {'match': true};
+}
 
 @Component({
     selector: 'my-signup',
@@ -19,8 +41,17 @@ export class CustomerComponent implements OnInit  {
         this.customerForm = this.fb.group({
             firstName: ['', [Validators.required, Validators.minLength(3)]],
             lastName: ['',  [Validators.required, Validators.maxLength(8)]],
-            email:    ['',     [Validators.required, Validators.pattern('[0-9a-zA-Z]+@[a-zA-Z0-9]+')]]
+            emailGroup: this.fb.group({
+                email:    ['',     [Validators.required, Validators.pattern('[0-9a-zA-Z]+@[a-zA-Z0-9]+')]],
+                confirmEmail: ['', Validators.required],
+            }, {validator: emailMatcher}),
+            phone: [],
+            notification: 'email',
+            rating: ['', ratingRange(1, 6)]
         });
+
+        this.customerForm.get('notification').valueChanges
+                        .subscribe((value) => this.setNotification(value));
     }
 
     populateData(): void {
@@ -30,9 +61,26 @@ export class CustomerComponent implements OnInit  {
         });
     }
 
+    getName(): void {
+        const fname = this.customerForm.get('firstName').value;
+        this.customerForm.patchValue({
+            firstName: fname.charAt(0).toUpperCase() + fname.slice(1)
+        });
+    }
     save() {
         console.log(this.customerForm);
         console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    setNotification(notifyVia: string): void {
+        const phoneControl = this.customerForm.get('phone');
+        if (notifyVia === 'phone') {
+            phoneControl.setValidators(Validators.required);
+        } else {
+            phoneControl.clearValidators();
+        }
+
+        phoneControl.updateValueAndValidity();
     }
  }
 
