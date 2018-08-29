fe2cc5df77ed2922d8d797acffbb492b4b614079
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-08-29 09:42:03 +0530

    a

---
+++ angular1/controller.js
@@ -0,0 +1,40 @@
+// IFFI
+(function(){
+    var app = angular.module("githubViewer", []);
+
+    var MainController = function($scope){
+        $scope.name= "Angular App"
+    }
+
+    app.controller("MainController", MainController)
+}())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function add(a,b){
+    return a+b
+}
+
+(function(a,b){return a+b}())     
+$scope
+$rootScope
+*/
