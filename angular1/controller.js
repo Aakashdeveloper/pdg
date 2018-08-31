b9850ddea9738932ab654b4354d003b4f553670e
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-08-31 09:50:12 +0530

    a

--- angular1/controller.js
+++ angular1/controller.js
@@ -2,8 +2,29 @@
 (function(){
     var app = angular.module("githubViewer", []);
 
-    var MainController = function($scope){
+    var Product = {
+        productName: "Video Game Controller",
+        productCode: "GMG-0042",
+        releaseDate: "October 15, 2015",
+        description: "Standard two-button video game controller",
+        price: 35.95,
+        starRating: 4.6,
+        imageUrl: "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+    }
+    var MainController = function($scope, $http){
+
+        var onUserComplete = function(response){
+            $scope.user = response.data
+        }
+
+        var onError = function(reason){
+            $scope.error = " Cloud not find user"
+        }
+        $http.get("https://api.github.com/users/aakashdeveloper")
+             .then(onUserComplete, onError)
+
         $scope.name= "Angular App"
+        $scope.product = Product;
     }
 
     app.controller("MainController", MainController)
